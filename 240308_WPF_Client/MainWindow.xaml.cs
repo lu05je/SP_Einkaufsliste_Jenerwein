7e61a6a2c0d91adf06aeff5427f4744a4a0612f6
Luca <luca.jenerwein@htl-saalfelden.at>
2024-05-28 15:23:39 +0200

    WPF Client Beginn

--- 240308_WPF_Client/MainWindow.xaml.cs
+++ 240308_WPF_Client/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +19,57 @@ namespace _240308_WPF_Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Produkt> produkte;
+
         public MainWindow()
         {
             InitializeComponent();
+            produkte = new ObservableCollection<Produkt>();
+            produktListBox.ItemsSource = produkte;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            string produktName = produktTextBox.Text.Trim();
+            if (!string.IsNullOrEmpty(produktName))
+            {
+                Produkt neuesProdukt = new Produkt { produkt = produktName };
+                SaveProdukt(neuesProdukt, (error) => {
+                    if (error == null)
+                    {
+                        Dispatcher.Invoke(() => produkte.Add(neuesProdukt));
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Fehler beim Speichern des Produkts: {error}");
+                    }
+                });
+            }
+        }
+
+       
+        private async void SaveProdukt(Produkt produkt, Action<string> callback)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                string json = JsonSerializer.Serialize(produkt);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync("http://10.10.2.79:8080/api/produkt", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    callback(null);
+                }
+                else
+                {
+                    callback(response.ReasonPhrase);
+                }
+            }
+            catch (Exception ex)
+            {
+                callback(ex.Message);
+            }
         }
     }
 }
---
+++ 240308_WPF_Client/Produkt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _240308_WPF_Client
+{
+    class Produkt
+    {
+        public string id { get; set; }
+        public bool status { get; set; }
+        public string produkt {  get; set; }
+
+
+    }
+}
